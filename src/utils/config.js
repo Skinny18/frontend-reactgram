6c4dceb170d6514267a814d37474bfb48cec1e16
Gabriel Carlos Carvalho <gabrielcarlos@MacBook-Pro-de-Gabriel.local>
2023-05-03 22:16:24 -0400

    api

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,6 +1,5 @@
-export const api = "http://localhost:5000/api"
-export const uploads = "http://localhost:5000/uploads"
-export const qr = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="
+export const api = "https://backend-reactgramaster.herokuapp.com/api"
+export const uploads = "https://backend-reactgramaster.herokuapp.com/uploads"
 
 export const requestConfig= (method, data, token = null, image = null) => {
     let config
